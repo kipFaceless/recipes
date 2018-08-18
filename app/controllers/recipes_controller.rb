59ca3cf0ce7cb37fbb5bffd5a399ceac7f7ef97d
Kip Faceless <kipfaceless@gmail.com>
2018-08-18 09:36:21 +0100

    Create and Edit Forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,33 @@ class RecipesController < ApplicationController
 	def index
 	@recipes = Recipe.all 	
 	end
+
+
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def update
+		@recipe = Recipe.find(params[:id])
+		recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost )
+		@recipe.update(recipe_params)
+		redirect_to @recipe
+	end
+
+	def new
+
+		@recipe = Recipe.new
+	end
+
+	def create
+		recipe_params = params.require(:recipe).permit!
+		@recipe = Recipe.new(recipe_params)
+		@recipe.save
+		redirect_to @recipe
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
   get "recipes" =>"recipes#index"
-  #resources :recipes
+  resources :recipes
 end
