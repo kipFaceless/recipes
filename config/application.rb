000cf0c39e29d3bb8b9cb0a114673c5dadf9bc1b
Kip Faceless <kipfaceless@gmail.com>
2018-09-11 13:27:58 +0100

    i18n

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,12 @@ module Receitasnet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Africa/Luanda'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = "pt-BR"
+    #i18n.config.available_locales = "pt-BR"
   end
 end
